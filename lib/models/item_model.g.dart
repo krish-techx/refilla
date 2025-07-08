90d1853d9da84d623fa8bc67fd723762bd573a22
Krishna Kumar <krish.techx@gmail.com>
2025-07-08 17:47:17 +0530

    feat: add local persistence using Hive

---
+++ lib/models/item_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ItemModelAdapter extends TypeAdapter<ItemModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  ItemModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ItemModel(
+      name: fields[0] as String,
+      quantity: fields[2] as int,
+      price: fields[1] as double,
+      unit: fields[3] as String,
+      image: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ItemModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.price)
+      ..writeByte(2)
+      ..write(obj.quantity)
+      ..writeByte(3)
+      ..write(obj.unit)
+      ..writeByte(4)
+      ..write(obj.image);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ItemModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
